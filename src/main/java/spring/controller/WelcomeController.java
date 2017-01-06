0e7b7f65fbc8f3d12154717273a477ac9f9d65f7
osamuoba <osamuoba@192.168.1.4>
2017-01-06 23:46:32 +0900

    xmlでthymeleaf化

--- src/main/java/spring/controller/WelcomeController.java
+++ src/main/java/spring/controller/WelcomeController.java
@@ -2,11 +2,12 @@ package spring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class WelcomeController {
 	
-	@RequestMapping("/")
+	@RequestMapping(value="/", method= {RequestMethod.GET, RequestMethod.POST})
 	public String home() {
 		return "index";
 	}
