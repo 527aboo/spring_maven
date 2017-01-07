4c2476d436953907652714977ec16b62f9480f48
osamuoba <osamuoba@192.168.1.4>
2017-01-07 19:34:52 +0900

    メッセージのプロパティファイル化

--- src/main/java/spring/form/echo/EchoForm.java
+++ src/main/java/spring/form/echo/EchoForm.java
@@ -5,11 +5,15 @@ import java.io.Serializable;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.Validator;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 
 public class EchoForm implements Serializable {
-	
-	@NotEmpty( message= "{echo.text.empty}")
-	@Size(max = 100, message= "{echo.text.max}")
+		
+	@NotEmpty(message="{NotEmpty.echoForm.text}")
+	@Size(max = 100, message= "{Size.echoForm.text}")
 	private String text;
 
 	public String getText() {
